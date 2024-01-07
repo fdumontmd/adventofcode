0f3d6e3b97bac6aa6290cec48f82bc7af0115082
Frederic Dumont <fdumontmd@gmail.com>
2024-01-07 12:44:44 +0900

    (perf) use Read instead of Int in z3 solver for 2023 24

--- 2023/day-24/solver.py
+++ 2023/day-24/solver.py
@@ -1,18 +1,19 @@
 import sys
 import z3
 
-x0 = z3.Int('x')
-y0 = z3.Int('y')
-z0 = z3.Int('z')
-a0 = z3.Int('a')
-b0 = z3.Int('b')
-c0 = z3.Int('c')
-sol = z3.Int('sol')
+# Int is shockingly slow; Real prints the solution
+# instantly
+x0 = z3.Real('x')
+y0 = z3.Real('y')
+z0 = z3.Real('z')
+a0 = z3.Real('a')
+b0 = z3.Real('b')
+c0 = z3.Real('c')
+sol = z3.Real('sol')
 
 s = z3.Solver()
 s.add(sol == x0 + y0 + z0)
 
-h = []
 c = 0
 
 for line in sys.stdin:
@@ -26,15 +27,20 @@ for line in sys.stdin:
     a1 = int(a1)
     b1 = int(b1)
     c1 = int(c1)
-    t = z3.Int("t" + str(c))
-    h.append([x1, y1, z1, a1, b1, c1])
-    s.add((x0 - x1) / (a1 - a0) > 0)
-    s.add(t * (a1 - a0) == (x0 - x1))
-    s.add(t * (b1 - b0) == (y0 - y1))
-    s.add(t * (c1 - c0)== (z0 - z1))
+    t = z3.Real("t" + str(c))
+    s.add(t >= 0)
+    s.add(x0 + t * a0 == x1 + t * a1)
+    s.add(y0 + t * b0 == y1 + t * b1)
+    s.add(z0 + t * c0 == z1 + t * c1)
     c += 1
+    if c > 8:
+        break
 
 s.check()
 m  = s.model()
+
+print(m[x0])
+print(m[y0])
+print(m[z0])
 
 print(m[sol])
