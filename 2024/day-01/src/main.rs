e29e5716940103c0934a22100007c81f44574b60
Frederic Dumont <fdumontmd@gmail.com>
2024-12-01 16:52:06 +0900

    2024 day 1 complete

---
+++ 2024/day-01/src/main.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+pub const INPUT: &str = include_str!("input.txt");
+
+fn parse(input: &str) -> (Vec<usize>, Vec<usize>) {
+    let mut left: Vec<usize> = vec![];
+    let mut right: Vec<usize> = vec![];
+
+    for line in input.lines() {
+        let nums: Vec<_> = line.split_whitespace().collect();
+        left.push(nums[0].parse().unwrap());
+        right.push(nums[1].parse().unwrap());
+    }
+    (left, right)
+}
+
+pub fn part1(input: &str) -> usize {
+    let (mut left, mut right) = parse(input);
+
+    left.sort();
+    right.sort();
+
+    left.into_iter()
+        .zip(right)
+        .map(|(l, r)| l.abs_diff(r))
+        .sum()
+}
+
+fn elt_count(data: impl IntoIterator<Item = usize>) -> HashMap<usize, usize> {
+    let mut counts = HashMap::new();
+
+    for i in data {
+        *counts.entry(i).or_insert(0) += 1;
+    }
+
+    counts
+}
+
+pub fn part2(input: &str) -> usize {
+    let (left, right) = parse(input);
+
+    let count = elt_count(right);
+
+    left.into_iter()
+        .map(|l| l * count.get(&l).unwrap_or(&0))
+        .sum()
+}
+
+fn main() {
+    println!("part 1: {}", part1(INPUT));
+    println!("part 2: {}", part2(INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+    const TEST_INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    #[rstest]
+    #[case(TEST_INPUT, 11)]
+    #[case(INPUT, 2904518)]
+    pub fn test_part1(#[case] input: &str, #[case] val: usize) {
+        assert_eq!(val, part1(input));
+    }
+
+    #[rstest]
+    #[case(TEST_INPUT, 31)]
+    pub fn test_part2(#[case] input: &str, #[case] val: usize) {
+        assert_eq!(val, part2(input));
+    }
+}
